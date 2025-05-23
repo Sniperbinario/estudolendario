8a49a2636e0565a991a2e2fc29423529c54e9f64
Sniperbinario <hiago.vinicius.b@gmail.com>
2025-05-22 23:27:28 -0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: './src',
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/index.html'),
-    },
-  },
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
