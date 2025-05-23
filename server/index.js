6785bcc8c43abdd0f93d5906de8256e4b5484199
Sniperbinario <hiago.vinicius.b@gmail.com>
2025-05-22 23:27:55 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 app.listen(PORT, () => {
