4c3a5340606267d6e356f0eb9e5d4296d4b5a604
Sniperbinario <hiago.vinicius.b@gmail.com>
2025-05-29 19:26:25 -0300

    Atualizar o App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -508,6 +508,7 @@ export default function App() {
         </div>
       </div>
     )
+    }
         // RETURN FINAL DA FUNÇÃO APP
     return (
       <div className="bg-gray-900 min-h-screen py-10 px-4 sm:px-8 text-white">
