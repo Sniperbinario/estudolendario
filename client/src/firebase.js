72ce664e5ef3d71277958912067a7000f3a41879
Sniperbinario <hiago.vinicius.b@gmail.com>
2025-05-30 16:51:52 -0300

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,15 +3,18 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "AIzaSyBHfn93lYERRYoorFIF_hh4nQYTElKDmfg",
   authDomain: "antiprocastinador.firebaseapp.com",
   projectId: "antiprocastinador",
-  storageBucket: "antiprocastinador.appspot.com",
+  storageBucket: "antiprocastinador.appspot.com", // <-- CORRIGIDO AQUI!
   messagingSenderId: "176220550517",
   appId: "1:176220550517:web:d933bbe5054dd1e1444f3c",
+  measurementId: "G-3R7YRFXR59"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
+// Exporta autenticação e banco de dados para uso no app
 export const auth = getAuth(app);
 export const db = getFirestore(app);
