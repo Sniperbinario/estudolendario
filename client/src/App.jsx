95bc1af011769d40b9c3fb1d87d27eceac3628d5
Sniperbinario <hiago.vinicius.b@gmail.com>
2025-06-15 22:37:25 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1268,14 +1268,14 @@ escolherMateria: (
             📘 Material de Apoio
           </button>
                   {mostrarConteudo && (
-                <div className="fixed inset-0 z-50 bg-black bg-opacity-70 flex items-center justify-center">
-               <div className="bg-gray-900 text-white p-8 rounded-2xl shadow-2xl max-w-3xl w-full max-h-[80vh] overflow-y-auto border border-white/10 relative">
-             <button
-              className="absolute top-4 right-4 text-2xl font-bold text-white bg-black bg-opacity-20 rounded-full w-10 h-10 flex items-center justify-center hover:bg-red-600 transition"
-              onClick={() => setMostrarConteudo(false)}
-             aria-label="Fechar"
-             >×</button>
-          <div className="prose prose-invert max-w-none">
+          <div className="fixed inset-0 z-50 bg-black bg-opacity-70 flex items-center justify-center">
+        <div className="bg-gray-900 text-white p-8 rounded-2xl shadow-2xl max-w-3xl w-full max-h-[80vh] overflow-y-auto border border-white/10 relative">
+        <button
+        className="absolute top-4 right-4 text-2xl font-bold text-white bg-black bg-opacity-20 rounded-full w-10 h-10 flex items-center justify-center hover:bg-red-600 transition"
+        onClick={() => setMostrarConteudo(false)}
+        aria-label="Fechar"
+      >×</button>
+      <div className="prose prose-invert max-w-none">
         <ReactMarkdown>
           {conteudosPF[blocoSelecionado.nome][blocoSelecionado.topico]}
         </ReactMarkdown>
@@ -1283,6 +1283,7 @@ escolherMateria: (
     </div>
   </div>
 )}
+
 
               {telaEscura && (
                 <div className="text-center mt-8">
