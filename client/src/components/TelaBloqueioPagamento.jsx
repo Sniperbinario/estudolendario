80c1aa1637b210cc9a9e400f4dab0c835e94b601
Sniperbinario <hiago.vinicius.b@gmail.com>
2025-06-19 23:54:29 -0300

    Update TelaBloqueioPagamento.jsx

--- client/src/components/TelaBloqueioPagamento.jsx
+++ client/src/components/TelaBloqueioPagamento.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import {
   salvarAcessoTemporario,
   temAcessoTemporario,
-  acessoLiberadoFirebase
 } from "../utils/controleAcesso";
 
 export default function TelaBloqueioPagamento() {
