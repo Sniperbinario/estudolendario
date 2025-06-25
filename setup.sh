0b819dd2c1e51ccad261c6718d015a6b0283aa99
Sniperbinario <hiago.vinicius.b@gmail.com>
2025-06-24 23:41:52 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Instala dependências do frontend
+cd client && npm install && cd ..
+
+# Instala dependências do backend
+cd server && npm install && cd ..
+
+# Se houver outras ferramentas, adicione seus comandos aqui
