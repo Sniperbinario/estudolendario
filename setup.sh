f029a69fabb0916b5b7d00857e9037decd0f609e
Sniperbinario <hiago.vinicius.b@gmail.com>
2025-06-24 23:48:50 -0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Instala dependências do frontend
-cd client && npm install && cd ..
-
-# Instala dependências do backend
-cd server && npm install && cd ..
-
-# Se houver outras ferramentas, adicione seus comandos aqui
